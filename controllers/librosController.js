887a4338eaca308c4599449665937d7de775b704
CinthiaElena <al193007@alumnos.uacj.mx>
2022-08-22 02:14:03 -0500

    CRUD base de datos

---
+++ controllers/librosController.js
@@ -0,0 +1,15 @@
+var con=require('../config/conexion')
+
+module.exports={
+
+    index:function(req,res){
+
+        con.query("SELECT * FROM libros", function(err,datos){
+            console.log(datos);
+        });
+
+        res.render('libros/index', { title: 'Aplicación CRUD' });
+
+    }
+
+}
