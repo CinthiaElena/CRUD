a6d37b4f1d057dc2193c200967d98e18f4152e80
CinthiaElena <60494641+CinthiaElena@users.noreply.github.com>
2022-08-28 21:53:41 -0500

    cambio de index en rutas

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path=require('path')
+var app = express();
 
 /* GET home page. */
 router.get('/', function(req, res, next){
-    res.send("Bienvenido a la biblioteca");
+    res.sendFile(path.resolve(__dirname,'index.html'))
 });
 
 module.exports = router;
